07cf0d9aa27e7758e511d0db9c25f9319f3e4b02
ibphuangchen <ibphuangchen@gmail.com>
2022-07-14 14:19:14 -0500

    edit snakemake configuration

--- work/0f/336f685543093827a3ba9dcc8632ac/.command.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ue
-mkdir peptide_level psm_level
-Rscript /Users/chuang8/proteomics/DeepRescore/bin/got_pga_input.R features.txt comet ./d2-rawPSMs.txt
-Rscript /Users/chuang8/proteomics/DeepRescore/bin/calculate_fdr.R ./ d2 /Users/chuang8/proteomics/DeepRescore/bin/protein.pro-ref.fasta
